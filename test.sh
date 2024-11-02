761b7e1a0f4993768bbb551900ae23cf00ac832c
obney <obney98@gmail.com>
2024-11-02 17:24:33 +0800

    first commit

---
+++ test.sh
@@ -0,0 +1,12 @@
+./JUSTIFY ./input/spla.blif ./output1101/spla-3_ori.txt 3
+./JUSTIFY ./input/spla.blif ./output1101/spla-4_ori.txt 4
+./JUSTIFY ./input/spla.blif ./output1101/spla-5_ori.txt 5
+./JUSTIFY ./input/spla.blif ./output1101/spla-6_ori.txt 6
+./JUSTIFY ./input/spla.blif ./output1101/spla-7_ori.txt 7
+./JUSTIFY ./input/spla.blif ./output1101/spla-8_ori.txt 8
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-3_flo.txt 3
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-4_flo.txt 4
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-5_flo.txt 5
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-6_flo.txt 6
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-7_flo.txt 7
+# ./JUSTIFY ./input/spla.blif ./output1101/spla-8_flo.txt 8
